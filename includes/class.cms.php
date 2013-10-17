82bc91c29b2a110f3efe140ea8217424f38f4517
Nickless <admin@acid-evo.com>
2013-10-17 10:21:33 +0200

    work started in class.cms.php..

--- includes/class.cms.php
+++ includes/class.cms.php
@@ -12,4 +12,70 @@ class Users extends Core {
         $this->core = &$core;
       }
 
+      public function userLogin($e,$p,$r){
+
+        $d = $this->core->Sql->prepare("SELECT * FROM `account` WHERE `email` = '$e'");
+        $d->execute();
+        $res = $d->fetch();
+
+        $s = $res['salt'];
+        $p = $this->generatePasswordHash($p,$s);
+
+        $d = $this->core->Sql->prepare("SELECT * FROM `account` WHERE `email` = '$e' AND `md5_encrypted_password` = '$p'");
+        $d->execute();
+        $res = $d->fetch();
+        $res = $this->generateObject($res);
+
+        $ip = $this->lockIp();
+
+        $this->banCheck('account',$res->id);
+        $this->banCheck('ip',$ip);
+
+        if(isset($res->id)){
+           // login success
+        }     
+      }
+
+      public function banCheck($type,$i){
+        if($type == "account"){
+            $d = $this->core->Sql->prepare("SELECT * FROM `account_banned` WHERE `id` = '$i'");
+            $d->execute();
+            if($d->row_count() < 0){
+                 die(ACCOUNT_BANNED);
+            }
+        }else{
+            $d = $this->core->Sql->prepare("SELECT * FROM `ip_banned` WHERE `ip` = '$i'");
+            $d->execute();
+            if($d->row_count() < 0){
+                 die(IP_BANNED);
+            }
+      }
+      }
+
+      public function lockIp(){
+         $s = $_SERVER;
+         switch($s){
+           case $s['REMOTE_ADDR']:
+             $x = $s['REMOTE_ADDR'];
+           break;
+
+           case $s['HTTP_X_FORWARDED_FOR']:
+             $x = $s['HTTP_X_FORWARDED_FOR'];
+           break;
+
+           case $s['HTTP_CLIENT_IP']:
+             $x = $s['HTTP_CLIENT_IP'];
+           break
+         }
+         return $x;
+      } 
+
+      public function generateObject($array){
+         $object = new ArrayObject($array, ArrayObject::ARRAY_AS_PROPS);
+         return $object;
+      }
+
+      public function generatePasswordHash($p,$s){
+        return md5(sha1($p,$s));
+      }
 }
--- languages/Danish.lang.php
+++ languages/Danish.lang.php
@@ -4,4 +4,6 @@ define('WELCOME','velkommen til');
 define('EMPTY_USERNAME','Husk at indtaste brugernavn');
 define('EMPTY_EMAIL','Husk at indtaste password');
 
+define('ACCOUNT_BANNED','Din konto er blevet blokeret, kontakt venligst admins på banned@'.$this->Config['domain_name']);
+define('IP_BANNED','Din ip er blevet blokeret, kontakt venligst admins på banned@'.$this->Config['domain_name']);
 ?>
